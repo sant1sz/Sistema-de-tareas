45bc84286d59867ff9bc3c74cd289fccfddb9d87
Santiago Sanchez <santiagosanchezramirez1@hotmail.com>
2026-09-22 00:26:31 -0500

    creación clase Prioridad y Tarea

---
+++ src/Prioridad.java
@@ -0,0 +1,25 @@
+public class Prioridad {
+    private String nombre;
+    private int valor;
+
+    public Prioridad(String nombre, int valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
---
+++ src/Tarea.java
@@ -0,0 +1,65 @@
+import java.util.Date;
+
+public class Tarea {
+    private String titulo;
+    private String descripcion;
+    private Date fecha_entrega;
+    private Prioridad prioridad;
+    private String estado;
+
+    public Tarea(String titulo, String descripcion, Date fecha_entrega, Prioridad prioridad, String estado) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fecha_entrega = fecha_entrega;
+        this.prioridad = prioridad;
+        this.estado = estado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha_entrega() {
+        return fecha_entrega;
+    }
+
+    public void setFecha_entrega(Date fecha_entrega) {
+        this.fecha_entrega = fecha_entrega;
+    }
+
+    public Prioridad getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Prioridad prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return  "titulo: " + titulo + "\n" +
+                "Descripcion: " + descripcion + "\n" +
+                "Fecha_entrega: " + fecha_entrega +"\n"+
+                "Prioridad: " + prioridad.getNombre();
+    }
+}
